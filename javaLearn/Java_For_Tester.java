08cc07c38ac040dc2966cc91be5f49fb2229323f
nphao281298 <nphao281298@gmail.com>
2025-02-21 13:46:12 +0700

    Java For Tester

---
+++ javaLearn/Java_For_Tester.java
@@ -0,0 +1,11 @@
+public class Java_For_Tester {
+    public static void main(String[] args) {
+        System.out.println(sumA(1,2));
+    }
+
+    public static int sumA(int a, int b){
+        int c = 0;
+        c = a+b;
+        return c;
+    }
+}
